0717df2e184a3f88d1dd626d3261e4cc160c40bd
KalpKanta <Exoplayz72@gmail.com>
2026-05-16 18:36:18 +0100

    Add files via upload

--- recommendation_system.py
+++ recommendation_system.py
@@ -7,7 +7,7 @@ movie = pd.read_csv("movies_metadata.csv")
 print(movie.head())
 print(movie.info())
 #weiogtted rating
-(v/v+m)*r+(m/v+m)*c
+#(v/v+m)*r+(m/v+m)*c
 #v = number of votes
 #m = minimum votes required to be listed in chart 
 #r = average rating for movie
@@ -17,5 +17,10 @@ m = movie["vote_count"].quantile(0.90)
 q_movies =  movie.copy().loc[movie["vote_count"]>=m]
 print(q_movies.shape)
 def wighted_rating(x,m=m,c=c):
-    pass
+    v = x["vote_count"]
+    r = x["vote_average"]
+    return  (v/(v+m)*r)+(m/(v+m)*c)
 
+q_movies["score"] = q_movies.apply(wighted_rating, axis = 1)
+q_movies.sort_values("score", ascending = False)
+print(q_movies[["title", "vote_count","vote_average","score"]].head(5))
